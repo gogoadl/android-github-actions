762aebac7633a8e239aa2c4d33772eeedb9d4d62
Adampark0030 <49335446+gogoadl@users.noreply.github.com>
2022-12-09 14:23:41 +0900

    initial commit

---
+++ app/src/main/java/com/hyeonwoo/android_github_actions/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.hyeonwoo.android_github_actions.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
